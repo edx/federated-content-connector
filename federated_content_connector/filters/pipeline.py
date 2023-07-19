7429960c405d213c68c4490135275ce39089ee5e
muhammad-ammar <mammar@gmail.com>
2023-07-19 14:42:44 +0500

    feat: add oep-50 hook filter pipeline step to return course metadata

--- federated_content_connector/filters/pipeline.py
+++ federated_content_connector/filters/pipeline.py
@@ -4,6 +4,7 @@ from openedx.core.djangoapps.catalog.utils import get_course_data
 from openedx_filters import PipelineStep
 
 from federated_content_connector.constants import EXEC_ED_COURSE_TYPE, EXEC_ED_LANDING_PAGE, PRODUCT_SOURCE_2U
+from federated_content_connector.models import CourseDetails
 
 
 class CreateCustomUrlForCourseStep(PipelineStep):
@@ -38,3 +39,42 @@ class CreateCustomUrlForCourseStep(PipelineStep):
                 course_home_url = getattr(settings, 'EXEC_ED_LANDING_PAGE', EXEC_ED_LANDING_PAGE)
 
         return {'course_key': course_key, 'course_home_url': course_home_url}
+
+
+
+class CourseMetadataRequestedStep(PipelineStep):
+    """
+    Step that provides the metadata for a course.
+
+    Example usage:
+
+    Add the following configurations to your configuration file:
+
+        "OPEN_EDX_FILTERS_CONFIG": {
+            "org.openedx.learning.course.metadata.requested.v1": {
+                "fail_silently": False,
+                "pipeline": [
+                    "federated_content_connector.filters.pipeline.CourseMetadataRequestedStep"
+                ]
+            }
+        }
+    """
+
+    def run_filter(self, course_key):  # pylint: disable=arguments-differ
+        """
+        Pipeline step that return the metadata for a course
+        """
+        try:
+            course_details = CourseDetails.objects.get(id=course_key)
+            data = {
+                "id": str(course_details.id),
+                "course_type": course_details.course_type,
+                "product_source": course_details.product_source,
+                "start_date": course_details.start_date,
+                "end_date": course_details.end_date,
+                "enroll_by": course_details.enroll_by,
+            }
+        except CourseDetails.DoesNotExist:
+            data = {}
+
+        return data
